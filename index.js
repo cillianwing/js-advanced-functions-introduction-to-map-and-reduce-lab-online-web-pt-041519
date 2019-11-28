d8b1d756123d82ba5392240662cc497e2565e68e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
 function mapToNegativize(sourceArray) {
-  
+  let arr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    arr.push(sourceArray[i] * -1)
+  }
+  return arr;
 }
