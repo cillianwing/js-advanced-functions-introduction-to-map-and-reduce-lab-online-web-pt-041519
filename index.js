1c36aad7cf09245aed05320f5a6af2a101dfc520
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ function mapToSquare(sourceArray) {
   return arr;
 }
 
-function reduceToTotal(sourceArray, startingPoint) {
-  
+function reduceToTotal(sourceArray, startingPoint=0) {
+  let total = startingPoint;
+  for (let i = 0; i < sourceArray.length; i++) {
+    total += sourceArray[i];
+  }
+  return total;
 }
