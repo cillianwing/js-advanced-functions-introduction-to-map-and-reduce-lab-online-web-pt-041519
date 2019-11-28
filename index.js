8667f37cd6103e60c44ffe4f87c7d2da11090e77
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-28 18:06:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function mapToNegativize(sourceArray) {
 }
 
 function mapToNoChange(sourceArray) {
-  
+  let arr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    arr.push(sourceArray[i]);
+  }
+  return arr;
 }
